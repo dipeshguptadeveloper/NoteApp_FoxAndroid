a2b86bb602f627d038584ce2761a2c30d1a6c2e6
Dipesh <dipeshguptadeveloper@gmail.com>
2023-08-09 11:31:40 +0530

    UI Initialization

--- app/src/main/java/com/dkgtech/notesappfoxandroid/MainActivity.kt
+++ app/src/main/java/com/dkgtech/notesappfoxandroid/MainActivity.kt
@@ -2,13 +2,40 @@ package com.dkgtech.notesappfoxandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.dkgtech.notesappfoxandroid.database.NoteDatabase
 import com.dkgtech.notesappfoxandroid.databinding.ActivityMainBinding
+import com.dkgtech.notesappfoxandroid.models.NoteModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    lateinit var database: NoteDatabase
+
+    lateinit var viewModel: NoteViewModel
+
+    lateinit var adapter: NotesAdapter
+
+    lateinit var selectedNote: NoteModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        with(binding) {
+
+            intUI()
+            viewModel =
+                ViewModelProvider(
+                    this@MainActivity,
+                    ViewModelProvider.AndroidViewModelFactory.getInstance(application).get(NoteView)
+                )
+
+        }
+
+    }
+
+    fun intUI() {
+
     }
 }
--- app/src/main/java/com/dkgtech/notesappfoxandroid/adapter/RecyclerNoteAdapter.kt
+++ app/src/main/java/com/dkgtech/notesappfoxandroid/adapter/RecyclerNoteAdapter.kt
@@ -3,11 +3,19 @@ package com.dkgtech.notesappfoxandroid.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.dkgtech.notesappfoxandroid.R
 import com.dkgtech.notesappfoxandroid.databinding.NoteRowBinding
 import com.dkgtech.notesappfoxandroid.models.NoteModel
+import kotlin.random.Random
 
-class RecyclerNoteAdapter(val context: Context, val noteList: ArrayList<NoteModel>) :
+class RecyclerNoteAdapter(
+    val context: Context,
+    val noteList: ArrayList<NoteModel>,
+    val fullList: ArrayList<NoteModel>,
+    val listner: notesItemClickListner
+) :
     RecyclerView.Adapter<RecyclerNoteAdapter.ViewHolder>() {
     class ViewHolder(val binding: NoteRowBinding) : RecyclerView.ViewHolder(binding.root) {
 
@@ -24,8 +32,60 @@ class RecyclerNoteAdapter(val context: Context, val noteList: ArrayList<NoteMode
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder.binding) {
             txtNoteTitle.text = noteList[position].title
+            txtNoteTitle.isSelected = true
             txtNoteDesc.text = noteList[position].desc
+            txtNoteDate.text = noteList[position].date
+            txtNoteDate.isSelected = true
+            cardLayout.setCardBackgroundColor(
+                holder.itemView.resources.getColor(
+                    randomColor(),
+                    null
+                )
+            )
 
+            cardLayout.setOnClickListener {
+                listner.onItemClicked(noteList[holder.adapterPosition])
+            }
+
+
+        }
+    }
+
+    fun randomColor(): Int {
+
+        val colorList = ArrayList<Int>()
+        colorList.add(R.color.black)
+        colorList.add(R.color.white)
+
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt(colorList.size)
+        return colorList[randomIndex]
+    }
+
+    interface notesItemClickListner {
+
+        fun onItemClicked(noteModel: NoteModel)
+        fun onLongItemClicked(noteModel: NoteModel, cardView: CardView)
+    }
+
+    fun updateList(newList: List<NoteModel>) {
+        fullList.clear()
+        fullList.addAll(newList)
+
+        noteList.clear()
+        noteList.addAll(fullList)
+        notifyDataSetChanged()
+    }
+
+    fun filterList(search: String) {
+
+        noteList.clear()
+        for (item in fullList) {
+            if (item.title.lowercase().contains(search.lowercase()) || item.desc.lowercase()
+                    .contains(search.lowercase())
+            ) {
+                noteList.add(item)
+            }
         }
     }
 }
